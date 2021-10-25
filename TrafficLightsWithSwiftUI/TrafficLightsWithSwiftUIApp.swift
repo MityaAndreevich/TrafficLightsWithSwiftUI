315a26ca7804a23a9ed3ee0327c7771996286780
MityaAndreevich <dmitry.logachev@icloud.com>
2021-10-25 23:06:35 +0300

    Initial Commit

---
+++ TrafficLightsWithSwiftUI/TrafficLightsWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightsWithSwiftUIApp.swift
+//  TrafficLightsWithSwiftUI
+//
+//  Created by Dmitry Logachev on 25.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightsWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
